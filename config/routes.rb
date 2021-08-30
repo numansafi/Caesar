6b3d0315bef7acd96f3c96189a60c4bd8853d947
Numan Safi <numansafi@live.com>
2021-08-30 17:32:39 +0100

    Started adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :venues only: [:index, :show] do
+  resources :venues, only: [:index, :show] do
     resources :bookings, except: [:destroy]
   end
   resources :bookings, only: [:destroy]
