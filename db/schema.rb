542ecece33c462e5d0b31dc634c2b292a7dcbb1a
Numan Safi <numansafi@live.com>
2021-09-03 00:32:12 +0100

    Updated database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_02_142047) do
+ActiveRecord::Schema.define(version: 2021_09_02_155347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,8 +42,6 @@ ActiveRecord::Schema.define(version: 2021_09_02_142047) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.date "date"
-    t.time "time"
     t.index ["user_id"], name: "index_bookings_on_user_id"
     t.index ["venue_id"], name: "index_bookings_on_venue_id"
   end
@@ -67,7 +65,6 @@ ActiveRecord::Schema.define(version: 2021_09_02_142047) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "first_name"
     t.string "last_name"
     t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
