d9f310e702f47372a0ff34343a57565feb209709
Numan Safi <numansafi@live.com>
2021-09-03 01:53:45 +0100

    Updated database

--- db/migrate/20210903004150_remove_user_info_from_users.rb
+++
@@ -1,7 +0,0 @@
-class RemoveUserInfoFromUsers < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :users, :first_name, :string
-    remove_column :users, :last_name, :string
-    remove_column :users, :username, :string
-  end
-end
