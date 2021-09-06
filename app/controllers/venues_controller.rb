a86f0409787e165514565b75ba6b8b7c2adcfd5a
Numan Safi <numansafi@live.com>
2021-09-06 15:30:25 +0100

    created filter on the index page

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -3,10 +3,17 @@ class VenuesController < ApplicationController
   before_action :set_review, only: [:show]
 
   def index
-    if params[:query].present?
-      @venues = Venue.search_by(params[:query])
+    if params["query"].present?
+      @filter = params["query"]["venue_type"].concat(params["query"]["venue_attribute"]).flatten.reject(&:blank?)
+      @venues = Venue.all.search_by("#{@filter}")
+    elsif params["search"].present?
+      @venues = Venue.search_by(params[:search])
     else
       @venues = Venue.all
+    end
+    respond_to do |format|
+      format.html
+      format.js
     end
   end
 
@@ -25,7 +32,7 @@ class VenuesController < ApplicationController
 
 
   def venue_params
-    params.require(:venue).permit(:name, :description, :address, :venue_type, :venue_attribute, )
+    params.require(:venue).permit(:name, :description, :address, :venue_type, :venue_attribute, :venue_type_list, :venue_attribute_list)
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,26 +8,26 @@
 #
 
 Booking.delete_all
-Venue.delete_all
 Review.delete_all
+Venue.delete_all
 User.delete_all
 
 puts "Starting seeding..."
 User.create(first_name: "test", last_name: "test", username: "test", email: "test@test.com", password: "123456")
 10.times do
 
-  venue_types = ["Indian", "Italian", "Chinese","Thai","Mexican","American"]
+  venue_types = Venue::VENUE_TYPES.sample
 
-  venue_attributes = ["Outdoor Space", "Disabled Toilets", "Disabled Access", "Disabled Parking", "Accommodating Staff", "Flexible Menu", "Dim Lighting", "Large Interior Space", "No Music", "Not Crowded", "Multiple Exits","No Strong Smells","Activities For Children","Mostly Families", "Outside Food Allowed"]
+  venue_attributes = Venue::VENUE_ATTRIBUTES.sample
 
   venue = Venue.new(
     name: Faker::Restaurant.name,
     description: Faker::Restaurant.description,
     address: Faker::Address.full_address,
-    venue_type: venue_types.sample,
-    venue_type_list: venue_types.sample,
-    venue_attribute: venue_attributes.sample,
-    venue_attribute_list: venue_attributes.sample
+    venue_type: venue_types,
+    venue_type_list: venue_types,
+    venue_attribute: venue_attributes,
+    venue_attribute_list: venue_attributes
   )
 
 venue.save!
