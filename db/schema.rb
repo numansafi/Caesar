cc86bb9ee8f5ba97a621ef30026b9d2437ab6d57
Numan Safi <numansafi@live.com>
2021-09-08 17:09:01 +0100

    implemeneted api for seed files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_06_114358) do
+ActiveRecord::Schema.define(version: 2021_09_08_152740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -109,6 +109,7 @@ ActiveRecord::Schema.define(version: 2021_09_06_114358) do
     t.string "venue_attribute"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 #
+require "json"
+require "open-uri"
 
 Booking.delete_all
 Review.delete_all
@@ -14,30 +16,66 @@ User.delete_all
 
 puts "Starting seeding..."
 User.create(first_name: "test", last_name: "test", username: "test", email: "test@test.com", password: "123456")
-10.times do
+# 10.times do
 
-  venue_types = Venue::VENUE_TYPES.sample
+  # venue_types = Venue::VENUE_TYPES.sample
 
   venue_attributes = Venue::VENUE_ATTRIBUTES.sample
 
-  venue = Venue.new(
-    name: Faker::Restaurant.name,
-    description: Faker::Restaurant.description,
-    address: Faker::Address.full_address,
-    venue_type: venue_types,
-    venue_type_list: venue_types,
-    venue_attribute: venue_attributes,
-    venue_attribute_list: venue_attributes
-  )
+#   venue = Venue.new(
+#     name: Faker::Restaurant.name,
+#     description: Faker::Restaurant.description,
+#     address: Faker::Address.full_address,
+#     venue_type: venue_types,
+#     venue_type_list: venue_types,
+#     venue_attribute: venue_attributes,
+#     venue_attribute_list: venue_attributes
+#   )
 
-venue.save!
-  Review.create!(
-    venue: venue,
-    rating: rand(0..5),
-    description: Faker::Restaurant.review,
-    venue_id: venue.id,
-    user: User.first
+# venue.save!
+#   Review.create!(
+#     venue: venue,
+#     rating: rand(0..5),
+#     description: Faker::Restaurant.review,
+#     venue_id: venue.id,
+#     user: User.first
+#   )
+# end
+
+
+# rm = RestClient.get "https://api.yelp.com/v3/businesses/search?location=london&categories=restaurants"
+
+
+# rm_array = JSON.parse(rm)[“businesses”]
+
+# rm_array.each do |venue|
+#  Venue.create(
+#   name: venue["name"],
+#   description: Faker::Restaurant.description,
+#   address: ["location"],
+#   venue_type: ["categories"],
+#   venue_type_list: ["categories"],
+#   venue_attribute: venue_attributes,
+#   venue_attribute_list: venue_attributes
+#   )
+# end
+
+url = "https://api.yelp.com/v3/businesses/search?location=london&term=restaurants&en_GB&limit=50&sort_by=best_match"
+data = URI.open(url, "Authorization" => "Bearer #{ENV['YELP_API_KEY']}").read
+venues = JSON.parse(data)["businesses"]
+venues.each do |venue|
+ Venue.create(
+  name: venue["name"],
+  description: Faker::Restaurant.description,
+  image: venue["image_url"],
+  address: venue["location"]["display_address"],
+  venue_type: venue["categories"][0]["title"],
+  venue_type_list: venue["categories"][0]["title"],
+  venue_attribute: venue_attributes,
+  venue_attribute_list: venue_attributes
   )
 end
 
-puts "finished seed!"
+puts venues
+
+https://www.yelp.com/biz/the-ivy-london?adjust_creative=4hvraeedlZ9pYeVgPfbnIQ&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=4hvraeedlZ9pYeVgPfbnIQ
